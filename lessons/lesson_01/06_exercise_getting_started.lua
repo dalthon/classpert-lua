77de3fccc3f36b855d452706c9b5ee5bf7b31cd7
Dalton <dalthon@dlt.local>
2022-07-05 17:57:42 +0200

    Solve lesson 1 exercise 06

--- lessons/lesson_01/06_exercise_getting_started.lua
+++ lessons/lesson_01/06_exercise_getting_started.lua
@@ -2,3 +2,16 @@
 
 -- Check if you have LPeg properly installed in your machine by redoing some
 -- examples from the previous lecture
+
+lpeg = require "lpeg"
+lu   = require "luaunit"
+
+function testPattern ()
+  local pattern = lpeg.P("teste")
+
+  lu.assertEquals(pattern:match("test"),   nil)
+  lu.assertEquals(pattern:match("teste"),  6  )
+  lu.assertEquals(pattern:match("testes"), 6  )
+end
+
+os.exit(lu.LuaUnit.run())
