0d810655e7c2bd59c6b6ed7dd3eb4a979c4af29b
Dalton <dalthon@dlt.local>
2022-07-05 18:41:00 +0200

    Solve lesson 1 exercise 14

--- lessons/lesson_01/14_exercise_adding_optional_sign_to_numerals.lua
+++ lessons/lesson_01/14_exercise_adding_optional_sign_to_numerals.lua
@@ -1,3 +1,36 @@
 -- Instructions
 
 -- Add an optional sign ("+") or ("-") to numerals
+
+lpeg = require "lpeg"
+lu   = require "luaunit"
+
+P  = lpeg.P
+R  = lpeg.R
+S  = lpeg.S
+C  = lpeg.C
+Cp = lpeg.Cp
+
+function squeezeNumber(number_and_spaces)
+  local result = string.gsub(number_and_spaces, "[ \n\t]", "")
+  return tonumber(result)
+end
+
+number   = R("09")
+unary_op = S("+-")
+plus     = P("+")
+space    = S(" \n\t")
+numbers  = ((unary_op * space^0 * number^1) + number^1) / squeezeNumber
+
+function testWithoutSpacesBetweenNumbers ()
+  local pattern = space^0 * numbers * space^0 * (Cp() * plus * space^0 * numbers * space^0)^0 * -1
+
+  lu.assertEquals( {pattern:match("+1")},     {1}        )
+  lu.assertEquals( {pattern:match("-1")},     {-1}       )
+  lu.assertEquals( {pattern:match("1 + 7")},  {1, 3, 7}  )
+  lu.assertEquals( {pattern:match("1 ++ 7")}, {1, 3, 7}  )
+  lu.assertEquals(  pattern:match("1 ++-7"),  nil        )
+  lu.assertEquals( {pattern:match("1 + -7")}, {1, 3, -7} )
+end
+
+os.exit(lu.LuaUnit.run())
