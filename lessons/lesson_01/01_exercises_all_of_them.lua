cf2b2b701f829b95140279f37ba755039ca0bc5e
Dalton <dalthon@dlt.local>
2022-07-06 19:32:43 +0200

    Add 1st lesson's exercises

---
+++ lessons/lesson_01/01_exercises_all_of_them.lua
@@ -0,0 +1,22 @@
+-- Exercise 1.1: Run the factorial example.
+-- What happens to your program if you enter a negative number?
+-- Modify the example to avoid this problem.
+
+-- Exercise 1.2: Run the `twice` example.
+-- Both by loading the file with the `-l` option and with `dofile`.
+-- Which way do you prefer?
+
+-- Exercise 1.3: Can you name other languages that use "--" for comments?
+
+-- Exercise 1.4: Which of the following strings are valid identifiers?
+-- ___ _end End end until? nil NULL one-step
+
+-- Exercise 1.5: What is the value of the expression `type(nil) == nil`?
+-- (You can use Lua to check your answer.) Can you explain this result?
+
+-- Exercise 1.6: How can you check whether a value is a Boolean without using the function `type`?
+
+-- Exercise 1.7: Consider the following expression: `(x and y and (not z)) or ((not y) and x)`
+-- Are the parentheses necessary? Would you recommend their use in that expression?
+
+-- Exercise 1.8: Write a simple script that prints its own name without knowing it in advance.
